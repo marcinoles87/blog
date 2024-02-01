c9fa5529c2dec2cecc46f87d8ef482b105175772
marcinoles87 <marcinoles87@gmail.com>
2024-02-01 16:26:05 +0100

    work with main section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import img1 from '../img/wakacje.jpg'
 
-function Header() {
+function  Header() {
+
+    const news = [
+        {
+            info: "sssssssssssss",
+            date: '22-12-2024',
+            img : img1 ,
+        }
+    ]
+    
+
   return (
-    <div>Header</div>
+    <div className='header-container'>
+        <div className='header-elements'>
+            {news.map( (item, index) => {
+                return(
+                    <div className='header-content' key={index} >
+                        <h3>{item.info}</h3>
+                        <p>{item.date}</p>
+                        <img src={item.img} alt={item.date}></img>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
-export default Header
+export default  Header
