5e02d95dcd2fb713485d3de0b076267f92b580ba
marcinoles87 <marcinoles87@gmail.com>
2024-02-01 16:08:36 +0100

    work with main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,9 @@
 import React from 'react'
+import '../App.css'
 
 function Main() {
   return (
-    <div className='main-contianer'>Main
-        <h1>Blog</h1>
-        <h2>About my life</h2>
-        <h3>Mrc 87</h3>
+    <div className='main-container'>
 
             <div className='main-link'>
                 <a href='#'>Link 1</a>
@@ -13,6 +11,15 @@ function Main() {
                 <a href='#'>Link 1</a>
                 <a href='#'>Link 1</a>
             </div>
+
+            <div className='main-info'>
+                <h1>Blog</h1>
+                <h2>About my life</h2>
+                <h3>Mrc 87</h3>
+            </div>
+       
+
+           
         
     </div>
   )
