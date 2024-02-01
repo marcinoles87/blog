5ba859d162aab9cea3b53ba2e6889e5217975b7e
marcinoles87 <marcinoles87@gmail.com>
2024-02-01 15:52:47 +0100

    work with code

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>Main
+        <h1>Blog</h1>
+        <h2>About my life</h2>
+        <h3>Mrc 87</h3>
+    </div>
+  )
+}
+
+export default Main
