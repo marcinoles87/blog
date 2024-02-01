07acd431875309860ce3a38ffc006efa5cfbe8e2
marcinoles87 <marcinoles87@gmail.com>
2024-02-01 16:01:13 +0100

    work with main section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import Article from './components/Article';
+import Header from './components/Header';
 import Main from './components/Main';
+import Navigation from './components/Navigation';
+import News from './components/News';
 
 function App() {
   return (
     <div className="App">
       <Main></Main>
+      <Header></Header>
+      <Navigation></Navigation>
+      <News></News>
+      <Article></Article>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main
+    <div className='main-contianer'>Main
         <h1>Blog</h1>
         <h2>About my life</h2>
         <h3>Mrc 87</h3>
+
+            <div className='main-link'>
+                <a href='#'>Link 1</a>
+                <a href='#'>Link 1</a>
+                <a href='#'>Link 1</a>
+                <a href='#'>Link 1</a>
+            </div>
+        
     </div>
   )
 }
