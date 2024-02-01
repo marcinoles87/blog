3fa4fde0a7c20967d02bc733adaf85cf38815a8a
marcinoles87 <marcinoles87@gmail.com>
2024-02-01 16:19:33 +0100

    work with main section

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../App.css'
+
 
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <div className='nav-container'>
+        <div className='nav-elements'>
+            <ul className='nav-link'>
+                <li><a href=''>My History</a></li>
+                <li><a href=''>My Life</a></li>
+                <li><a href=''>My Hobby</a></li>
+                <li><a href=''>My Mind</a></li>
+               
+            </ul>
+        </div>
+        </div>
   )
 }
 
