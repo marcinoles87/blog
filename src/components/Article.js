f96fc6fc4f5dc1148c05e530866f5cbc566920d2
marcinoles87 <marcinoles87@gmail.com>
2024-02-02 14:51:28 +0100

    work with header section

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../App.css'
+import myFoto from '../img/wakacje.jpg'
 
 function Article() {
   return (
-    <div>Article</div>
+    <div className='article-bio-container'>
+      <h1> My Full Name</h1>
+      <p> Little slogan</p>
+      <img src={myFoto} alt='my-foto'></img>
+
+      <div className='article-links'>
+        <a href='#'>lnk1</a>
+        <a href='#'>lnk1</a>
+        <a href='#'>lnk1</a>
+      </div>
+    </div>
+    
   )
 }
 
