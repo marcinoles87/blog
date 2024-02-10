7fa9b861e2ad39a2b120fc152fd93daf21fdf596
marcinoles87 <marcinoles87@gmail.com>
2024-02-10 20:47:21 +0100

    work with react router

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className='skills-container'>
+      <div>
+        <h1>My carrer</h1>
+      </div>
+
+      <div>
+        <h1>My Skill it</h1>
+      </div>
+
+      <div>
+        <h1>My strong side</h1>
+      </div>
+    </div>
   )
 }
 
