8e2f25c4da8ea5d9002cb4f19add063cc87ebdd4
marcinoles87 <marcinoles87@gmail.com>
2024-02-11 08:41:13 +0100

    work with skill section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -5,14 +5,32 @@ function Skills() {
     <div className='skills-container'>
       <div>
         <h1>My carrer</h1>
+        <ul>
+          <li>Kierownik magazynu 2012-2024</li>
+          <li>Zaopatrzeniowiec 2008-2012</li>
+       
+        </ul>
       </div>
 
       <div>
         <h1>My Skill it</h1>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>JavaScript</li>
+        </ul>
       </div>
 
       <div>
         <h1>My strong side</h1>
+        <ul>
+          <li>Komunikatywnosc</li>
+          <li>Pracowitosc</li>
+          <li>Team Player</li>
+          <li>Zaangazowanie</li>
+          <li>Multi-tasking</li>
+        </ul>
       </div>
     </div>
   )
