19ef1a428815e43524c3449affbaca4e7a932d3a
marcinoles87 <marcinoles87@gmail.com>
2024-02-13 17:07:09 +0100

    work with skill section

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -11,6 +11,7 @@ function Skills() {
     skillList.classList.toggle('show-list');
   }
   return (
+    <>
     <div className='skills-container'>
 
       
@@ -24,28 +25,38 @@ function Skills() {
         </ul>
       </div>
       </div>
-/* 
-      <div>
-      <i class="fa-solid fa-laptop-code fa-2xl"></i>
-        <ul className='skill-list' onMouseEnter={showSkill}>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React</li>
-          <li>JavaScript</li>
-        </ul>
-      </div>
 
-      <div>
-      <i class="fa-solid fa-user fa-2xl"></i>
-        <ul className='side-list' onMouseEnter={showSkill}>
-          <li>Komunikatywnosc</li>
-          <li>Pracowitosc</li>
-          <li>Team Player</li>
-          <li>Zaangazowanie</li>
-          <li>Multi-tasking</li>
-        </ul>
-      </div>
-    </div> */
+<div className='skills-container'>
+
+      
+<i class="fa-solid fa-briefcase fa-2xl"></i>
+
+<div className='skill-card'>
+ <ul className='skill-list'>
+  <li>Kierownik magazynu 2012-2024</li>
+  <li>Zaopatrzeniowiec 2008-2012</li>
+
+</ul>
+</div>
+</div>
+
+<div className='skills-container'>
+
+      
+<i class="fa-solid fa-briefcase fa-2xl"></i>
+
+<div className='skill-card'>
+ <ul className='skill-list'>
+  <li>Kierownik magazynu 2012-2024</li>
+  <li>Zaopatrzeniowiec 2008-2012</li>
+
+</ul>
+</div>
+</div>
+
+</>
+
+
   )
 }
 
